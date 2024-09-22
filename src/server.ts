dc3fd93b13badb7d4bf909b032c3c9efc9744a58
Lucas Farias <lucas.farias@rtbhouse.com>
2024-09-22 11:12:13 -0300

    fix render host missing

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import { app } from './app'
 import { env } from './env'
 
+const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
+const port = env.PORT
+
 app
   .listen({
-    port: env.PORT,
+    host,
+    port,
   })
   .then(() => {
-    console.log('HTTP server running...')
+    console.log(`HTTP server running on ${host}:${port}...`)
   })
